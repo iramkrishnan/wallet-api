29149be44b9c56f99ac2ee62df36286d495a5cdb
Ram Krishnan <ramkrishnan@live.com>
2022-05-17 19:05:06 +0530

    Add Transaction credit/debit API

--- app/controllers/transaction/index.js
+++ app/controllers/transaction/index.js
@@ -1,8 +1,43 @@
-const addTransaction = async (req, res) => {};
+const WalletModel = require('../../models/Wallet');
+const TransactionModel = require('../../models/Transaction');
+const responseUtility = require('../../utils/responseUtility');
+const { DataNotFoundException } = require('../../utils/exceptions');
+const { roundDecimal } = require('../../utils/number');
 
-const getTransactions = async (req, res) => {};
+const addTransaction = async (req, res, next) => {
+  const { amount, description } = req.body;
+  const { walletId } = req.params;
+
+  try {
+    const wallet = await WalletModel.readOneByKey({ _id: walletId });
+    if (!wallet) throw new DataNotFoundException('Wallet Not Found');
+
+    const transaction = await TransactionModel.create({
+      wallet: walletId,
+      balance: roundDecimal(wallet.balance + amount),
+      description,
+      amount: Math.abs(amount),
+      type: amount > 0 ? 'CREDIT' : 'DEBIT',
+      date: new Date(),
+    });
+
+    const updatedWallet = await WalletModel.update(
+      { _id: walletId },
+      { balance: roundDecimal(wallet.balance + amount) },
+      { new: true }
+    );
+
+    const response = {
+      balance: updatedWallet.balance,
+      transactionId: transaction._id,
+    };
+
+    return res.status(200).json(responseUtility.build('SUCCESS', response));
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = {
   addTransaction,
-  getTransactions,
 };
---
+++ app/utils/number.js
@@ -0,0 +1,5 @@
+const roundDecimal = (number) => Math.round(number * 10000) / 10000;
+
+module.exports = {
+  roundDecimal,
+};
