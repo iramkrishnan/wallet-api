35df8cad6a6e77426c8e7fdc89f0b27ea4e0326f
Ram Krishnan <ramkrishnan@live.com>
2022-05-17 18:59:15 +0530

    Add Setup Wallet API

--- app/controllers/wallet/index.js
+++ app/controllers/wallet/index.js
@@ -1,8 +1,39 @@
-const setupWallet = async (req, res) => {};
+const WalletModel = require('../../models/Wallet');
+const TransactionModel = require('../../models/Transaction');
+const responseUtility = require('../../utils/responseUtility');
 
-const getWalletDetails = async (req, res) => {};
+const setupWallet = async (req, res, next) => {
+  try {
+    const wallet = await WalletModel.create({
+      name: req.body.name,
+      balance: req.body.balance,
+      date: new Date(),
+    });
+
+    const { _id: walletId, balance, name, date } = wallet;
+
+    const { _id: transactionId } = await TransactionModel.create({
+      wallet: walletId,
+      balance,
+      amount: balance,
+      type: 'CREDIT',
+      date: new Date(),
+    });
+
+    const response = {
+      id: walletId,
+      balance,
+      transactionId,
+      name,
+      date,
+    };
+
+    return res.status(200).json(responseUtility.build('SUCCESS', response));
+  } catch (err) {
+    return next(err);
+  }
+};
 
 module.exports = {
   setupWallet,
-  getWalletDetails,
 };
