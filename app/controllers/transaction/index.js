45261a705ddc013e4a747b157b49feab7e6a696b
Ram Krishnan <ramkrishnan@live.com>
2022-05-17 23:48:34 +0530

    Code refactor

--- app/controllers/transaction/index.js
+++ app/controllers/transaction/index.js
@@ -2,13 +2,10 @@ const responseUtility = require('../../utils/responseUtility');
 const transactionService = require('../../services/transactionService');
 
 const addTransaction = async (req, res, next) => {
-  const { amount, description } = req.body;
-  const { walletId } = req.params;
   try {
     const response = await transactionService.addTransaction({
-      amount,
-      description,
-      walletId,
+      ...req.body,
+      ...req.params,
     });
     return res.status(200).json(responseUtility.build('SUCCESS', response));
   } catch (err) {
